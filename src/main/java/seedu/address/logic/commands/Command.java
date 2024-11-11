cd17fe935a2ca8028bbbd7726d81d6846c234d26
ruiming97 <e1130094@u.nus.edu>
2024-11-11 22:16:30 +0800

    Remove unnecessary empty lines

--- src/main/java/seedu/address/logic/commands/Command.java
+++ src/main/java/seedu/address/logic/commands/Command.java
@@ -8,8 +8,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Person;
 
-
-
 /**
  * Represents a command with hidden internal logic and the ability to be executed.
  */
